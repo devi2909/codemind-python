e579f048415eaba965fcd2af437b89ae4ca7d5fa
devi2909 <98701535+devi2909@users.noreply.github.com>
2022-06-28 01:02:02 +0530

    n=int(input()) m=int(input()) for i in range (n,m+1):     c=0     for j in range(1,i+1):         if(i%j==0):             c+=1     if(c==2):         print(i)

--- Generate_Prime_Numbers_In_A_Interval_.py
+++ Generate_Prime_Numbers_In_A_Interval_.py
@@ -1,14 +1,9 @@
-def fun(n):
+n=int(input())
+m=int(input())
+for i in range (n,m+1):
     c=0
-    for i in range(1,n+1):
-        if(n%i==0):
+    for j in range(1,i+1):
+        if(i%j==0):
             c+=1
-    if c==2:
-        return 1
-    else:
-        return 0
-a=int(input())
-b=int(input())
-for i in range(a,b+1):
-    if(fun(i)):
+    if(c==2):
         print(i)
