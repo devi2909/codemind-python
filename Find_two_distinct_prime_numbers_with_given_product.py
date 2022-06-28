d20b6582a23f7b0a08e856d03738640a54be977e
devi2909 <98701535+devi2909@users.noreply.github.com>
2022-06-28 11:10:53 +0530

    def prime(n):     if n==1:         return 0     for i in range(2,int(n**0.5)+1):         if n%i==0:             return 0     return 1 x=int(input()) c=0 for i in range(1,x):     if x%i==0:         if prime(i):             print(i,end=' ')             c+=1 if c==0:     print('-1')

--- Find_two_distinct_prime_numbers_with_given_product.py
+++ Find_two_distinct_prime_numbers_with_given_product.py
@@ -1,44 +1,18 @@
-from math import *
-def SieveOfEratosthenes(n, isPrime) :
-	isPrime[0], isPrime[1] = False, False
+def prime(n):
+    if n==1:
+        return 0
+    for i in range(2,int(n**0.5)+1):
+        if n%i==0:
+            return 0
+    return 1
+x=int(input())
+c=0
+for i in range(1,x):
+    if x%i==0:
+        if prime(i):
+            print(i,end=' ')
+            c+=1
+if c==0:
+    print('-1')
 
-	for i in range(2, n + 1) :
-		isPrime[i] = True
-
-	for p in range(2, int(sqrt(n)) + 1) :
-		if isPrime[p] == True :
-
-			for i in range(p * 2, n + 1, p) :
-				isPrime[i] = False
-
-# Function to print a prime pair
-# with given product
-def findPrimePair(n) :
-
-	flag = 0
-	
-	# Generating primes using Sieve
-	isPrime = [False] * (n + 1)
-	SieveOfEratosthenes(n, isPrime)
-
-	# Traversing all numbers to
-	# find first pair
-	for i in range(2, n) :
-		x = int(n / i)
-
-		if (isPrime[i] & isPrime[x] and
-			x != i and x * i == n) :
-			print(i, x)
-			flag = 1
-			break
-
-	if not flag :
-		print("-1")
-	
-# Driver code	
-if __name__ == "__main__" :
-
-	# Function calling
-	n = int(input())
-
-	findPrimePair(n)
+    
