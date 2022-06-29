82854b04b8219df88423c650517539e88c403401
devi2909 <98701535+devi2909@users.noreply.github.com>
2022-06-29 21:47:30 +0530

    n = int(input()) x = str(n) op = [x] for i in range(len(x)):     if x[i] == "9":         z = x[:i] + "6" + x[1+i:]     elif x[i] == "6":         z = x[:i] + "9" + x[1+i:]     op.append(z) op.sort() print(op[-1])

--- Maximum_69_Number.py
+++ Maximum_69_Number.py
@@ -1,7 +1,11 @@
-s = input()
-l = list(s)
-for i in range(len(l)):
-    if l[i] == '6':
-        l[i] = '9'
-        break
-print(''.join(l))
+n = int(input())
+x = str(n)
+op = [x]
+for i in range(len(x)):
+    if x[i] == "9": 
+        z = x[:i] + "6" + x[1+i:]
+    elif x[i] == "6":
+        z = x[:i] + "9" + x[1+i:]
+    op.append(z)
+op.sort()
+print(op[-1])
